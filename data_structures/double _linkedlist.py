a946e8187f258c40e08d5d9cf53381aa3d238fd1
Larry122445 <korlilarryaddo43@gmail.com>
2024-05-11 16:03:11 -1200

    new commit

---
+++ data_structures/double _linkedlist.py
@@ -0,0 +1,54 @@
+class ListNode:
+    def __init__(self, val=0, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_at_head(self, val):
+        new_node = ListNode(val)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def add_at_tail(self, val):
+        new_node = ListNode(val)
+        if not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def remove_element(self, val):
+        current = self.head
+        while current:
+            if current.val == val:
+                if current.prev:
+                    current.prev.next = current.next
+                else:
+                    self.head = current.next
+                
+                if current.next:
+                    current.next.prev = current.prev
+                else:
+                    self.tail = current.prev
+                return
+            current = current.next
+        print(f"Element {val} not found in the list.")
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.val, end=" ")
+            current = current.next
+        print()
